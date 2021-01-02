1cf1bfea5962f7c9726e0cf809b762b3b2655225
Sean Bolton <sean@smbolton.com>
2021-01-01 16:37:02 -0800

    Release version 1.1.1

--- src/gui_widgy_editor.c
+++ src/gui_widgy_editor.c
@@ -1,6 +1,6 @@
 /* hexter DSSI software synthesizer GUI
  *
- * Copyright (C) 2011, 2012, 2018 Sean Bolton.
+ * Copyright (C) 2011, 2012, 2018, 2021 Sean Bolton.
  *
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License as
